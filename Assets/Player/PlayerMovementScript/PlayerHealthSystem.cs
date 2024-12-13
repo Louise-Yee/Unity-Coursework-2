a15852fd6961826557b15eb11fe28fb28db08095
hashboy05 <AOHuang05@gmail.com>
2024-12-13 21:01:05 +0800

    added revive feature, third map

--- Assets/Player/PlayerMovementScript/PlayerHealthSystem.cs
+++ Assets/Player/PlayerMovementScript/PlayerHealthSystem.cs
@@ -1,21 +1,26 @@
 using System.Collections;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerHealthSystem : MonoBehaviour
 {
     public int maxHealth = 10; // Maximum health
     private int currentHealth; // Current health
     public float pushForce = 2f; // Force to push the zombie away
-    public float revivalTime = 5f; // Time required to revive the player
+    private float revivalTime = 5f; // Time required to revive the player
+    private float revivalCount = 0f;
     public int reviveHealth = 5; // Health restored upon revival
 
     public bool isPlayerOne;
     private bool isDowned = false; // Tracks if the player is downed
-    private bool isBeingRevived = false; // Tracks if the player is being revived
-
     private Player1Movement player1Movement;
     private Player2Movement player2Movement;
     private Animator animator;
+    public GameObject revivalPrompt; // UI element for revival prompt
+    private PlayerHealthSystem nearbyAlivePlayer;
+    [SerializeField] TextMeshProUGUI text;
+    [SerializeField] Image image;
 
     void Start()
     {
@@ -31,6 +36,7 @@ public class PlayerHealthSystem : MonoBehaviour
 
         // Initialize the player's health
         currentHealth = maxHealth;
+        revivalPrompt.SetActive(false); // Hide prompt initially
     }
 
     void Update()
@@ -39,6 +45,14 @@ public class PlayerHealthSystem : MonoBehaviour
         if (currentHealth <= 0 && !isDowned)
         {
             EnterDownedState();
+        }
+
+        // Check for revival input
+        if (nearbyAlivePlayer!=null){
+            if (nearbyAlivePlayer.isDowned && Input.GetKey(KeyCode.R))
+            {
+                StartRevival();
+            }
         }
     }
 
@@ -50,12 +64,38 @@ public class PlayerHealthSystem : MonoBehaviour
             currentHealth--;
             // Debug.Log($"Player hit by Zombie! Health: {currentHealth}");
         }
+
+        // Check for nearby downed player
+        PlayerHealthSystem otherPlayerHealth = other.GetComponent<PlayerHealthSystem>();
+        if (other.CompareTag("Player") && otherPlayerHealth != null && otherPlayerHealth.isDowned)
+        {
+            text.gameObject.SetActive(true);
+            image.gameObject.SetActive(true);
+            nearbyAlivePlayer = otherPlayerHealth;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            PlayerHealthSystem otherPlayerHealth = other.GetComponent<PlayerHealthSystem>();
+            if (otherPlayerHealth != null && otherPlayerHealth.isDowned)
+            {
+                image.fillAmount = 1;
+                text.gameObject.SetActive(false);
+                image.gameObject.SetActive(false);
+                nearbyAlivePlayer = null;
+            }
+            revivalCount = 0;
+        }
     }
 
     private void EnterDownedState()
     {
         isDowned = true;
         Debug.Log("Player is downed!");
+        ReviveProgress.targetObject = transform;
 
         // Reduce movement speed for the downed state
         if (isPlayerOne)
@@ -72,42 +112,31 @@ public class PlayerHealthSystem : MonoBehaviour
 
     public void StartRevival()
     {
-        if (isDowned && !isBeingRevived)
-        {
-            Debug.Log("Player is being revived...");
-            isBeingRevived = true;
+        Debug.Log("Player is being revived...");
+        nearbyAlivePlayer.RevivePlayer();
+    }
+
+    private void RevivePlayer()
+    {
+        revivalCount += Time.deltaTime;
+        image.fillAmount = revivalCount/revivalTime;
+        if (revivalCount >= revivalTime){
+            // Revive the player
+            isDowned = false;
+            currentHealth = reviveHealth;
             if (isPlayerOne)
             {
-                player1Movement.isBeingRevived();
+                player1Movement.isRevived(); // Restore movement speed
             }
             else
             {
-                player2Movement.isBeingRevived();
+                player2Movement.isRevived();
             }
-
-            // Start revival coroutine
-            StartCoroutine(RevivePlayer());
+            animator.SetBool("isDown", false);
+            revivalCount = 0;
+            ReviveProgress.targetObject = null;
+            text.gameObject.SetActive(false);
+            image.gameObject.SetActive(false);
         }
-    }
-
-    private IEnumerator RevivePlayer()
-    {
-        yield return new WaitForSeconds(revivalTime);
-
-        // Revive the player
-        isBeingRevived = false;
-        isDowned = false;
-        currentHealth = reviveHealth;
-        if (isPlayerOne)
-        {
-            player1Movement.isRevived(); // Restore movement speed
-        }
-        else
-        {
-            player2Movement.isRevived();
-        }
-        animator.SetBool("isDown", false);
-
-        // Debug.Log($"Player revived with {currentHealth} health!");
     }
 }
